b51011baf638b0af64eb01d095f40fc799820003
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-02 19:58:13 -0300

    Testing HTT methods

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,21 +2,44 @@ import express, { response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const users = [
   'Adriano',
   'Grace',
   'Thor',
-  'Joaz'
+  'Joaz',
+  'Mariano'
 ];
 
 app.get('/users', (request, response) => {
-  response.json(users);
+  const search: any = request.query.search;
+
+  const filteredUsers = search ? users.filter(user => user.includes(String(search))) : users;
+
+  response.json(filteredUsers);
 });
 
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  let user = users[id];
+
+  if (!user) {
+    user = 'Usuário não encontrado';
+  }
+
+  return response.json(user);
+})
+
 app.post('/users', (request, response) => {
+  const data = request.body;
+
+
+
   const user = {
-    name: 'Adriano',
-    email: 'siva.souza.adriano@gmail.com'
+    name: data.name,
+    email: data.email
   };
 
   return response.json(user);
