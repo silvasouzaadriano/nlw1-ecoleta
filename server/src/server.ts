095e4c1e7a20874864d7db4e40dbffba0b25a3b8
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-02 20:29:16 -0300

    Configurating database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,50 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const users = [
-  'Adriano',
-  'Grace',
-  'Thor',
-  'Joaz',
-  'Mariano'
-];
-
-app.get('/users', (request, response) => {
-  const search: any = request.query.search;
-
-  const filteredUsers = search ? users.filter(user => user.includes(String(search))) : users;
-
-  response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  let user = users[id];
-
-  if (!user) {
-    user = 'Usuário não encontrado';
-  }
-
-  return response.json(user);
-})
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-
-
-
-  const user = {
-    name: data.name,
-    email: data.email
-  };
-
-  return response.json(user);
-
-});
+app.use(routes);
 
 app.listen(3333);
 
