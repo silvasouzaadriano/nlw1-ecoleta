d1ede9ef7fc4a3fef68d0f09cf07989f3103aebc
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-03 00:28:53 -0300

    Configurating the Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
