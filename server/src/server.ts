a5ae5d184afbd02674a0c0c4f1f38941eb386f0c
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-04 12:41:33 -0300

    Created Ponto de Coleta HTML

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(cors);
-
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(cors);
 
 app.listen(3333);
 
