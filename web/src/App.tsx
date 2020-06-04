04524a763f05ddc2270dc158bbc91ded214415ff
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-04 00:49:17 -0300

    Created HTML of first page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const handleButtonClick = () => {
-    setCounter(counter + 1);
-  }
 
   return (
     <>
-      <Header title={`Contador: ${counter}`} />
-
-      <h1>{counter * 2}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      <Home />
     </>
   );
 }
