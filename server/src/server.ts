7942bd68109ca8c8d2df4ddbf6be521a784080aa
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-06 23:17:06 -0300

    Data validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-
 import routes from './routes';
+import { errors } from 'celebrate';
 
 
 const app = express();
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
 
